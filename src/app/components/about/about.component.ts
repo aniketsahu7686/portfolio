f16da5586a25ec82fd82f51a1a9c4aa477b8da21
aniketsahu7686 <aniketsahu1909@gmail.com>
2026-02-15 12:26:31 +0530

    sunday third commit

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, AfterViewInit, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -8,6 +8,26 @@ import { CommonModule } from '@angular/common';
     templateUrl: './about.component.html',
     styleUrl: './about.component.scss'
 })
-export class AboutComponent {
+export class AboutComponent implements AfterViewInit {
+    isRevealed = false;
+    @ViewChild('aboutContent') aboutContent!: ElementRef;
 
+    ngAfterViewInit() {
+        const observer = new IntersectionObserver(
+            (entries) => {
+                entries.forEach((entry) => {
+                    if (entry.isIntersecting) {
+                        this.isRevealed = true;
+                        observer.disconnect();
+                    }
+                });
+            },
+            { threshold: 0.2 }
+        );
+
+        const section = document.getElementById('about');
+        if (section) {
+            observer.observe(section);
+        }
+    }
 }
