9839210444b3a03f2fd670d315768aced1d2e416
aniketsahu7686 <aniketsahu1909@gmail.com>
2026-02-15 21:50:28 +0530

    15th feb 7th commit

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -10,4 +10,8 @@ import { CommonModule } from '@angular/common';
 })
 export class FooterComponent {
     currentYear = new Date().getFullYear();
+
+    scrollToTop() {
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+    }
 }
